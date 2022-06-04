f8a8a726a85f52cf05a5bd8b22c8e768b6c4d0c5
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2022-06-04 10:04:35 +0300

    Update odev4.sql

--- odev4.sql
+++ odev4.sql
@@ -3,10 +3,10 @@
 --film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
 SELECT DISTINCT replacement_cost FROM film;
 
-film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+--film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
 SELECT COUNT(DISTINCT replacement_cost) FROM film;
 
--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 SELECT COUNT(title) FROM film
 WHERE title LIKE 'T%' AND rating='G';
 
